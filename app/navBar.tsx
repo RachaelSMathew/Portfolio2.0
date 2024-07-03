e260ae2c6333777f91b4c14167c6f40c4b6cc2f6
Rachael Mathew <rachaelmathew1@gmail.com>
2024-07-03 16:01:35 -0400

    Update navBar.tsx

--- app/navBar.tsx
+++ app/navBar.tsx
@@ -7,35 +7,34 @@ export default function NavBar() {
     const [changeNav, setChangeNav] = useState(false)
     const [windowSize, setWindowSize] = useState([0,0]);
     
-  const [linkedinHover, setLinkedInHover] = useState(0)
-  const [intstaHover, setInstaHover] = useState(0)
-  const [fbHover, setFBHover] = useState(0)
-
-  useEffect(() => {
-    if (typeof window === "undefined") return
-    setWindowSize([window.innerHeight, window.innerWidth]);
-    setChangeNav(window.innerWidth<1300 && window.innerWidth > 650)
-
-    const windowSizeHandler = () => {
+    const [linkedinHover, setLinkedInHover] = useState(0)
+    const [intstaHover, setInstaHover] = useState(0)
+    const [fbHover, setFBHover] = useState(0)
+    
+    useEffect(() => {
         if (typeof window === "undefined") return
         setWindowSize([window.innerHeight, window.innerWidth]);
-        setChangeNav(window.innerWidth<1300 && window.innerWidth > 650)
-    };
+        setChangeNav(window.innerWidth<1300)
+        const windowSizeHandler = () => {
+          if (typeof window === "undefined") return
+          setWindowSize([window.innerHeight, window.innerWidth]);
+          setChangeNav(window.innerWidth<1300)
+        };
 
-    window.addEventListener("resize", windowSizeHandler);
+        window.addEventListener("resize", windowSizeHandler);
 
-    return () => {
-        window.removeEventListener("resize", windowSizeHandler);
-    }; 
-  }, []);
+        return () => {
+          window.removeEventListener("resize", windowSizeHandler);
+        };
+    }, []);
 
     return (
-        <view style={{width: windowSize[1], height: windowSize[0], top: changeNav ? 29:0, display: "flex", position: "fixed", alignItems: "flex-end", justifyContent: "space-around", flexDirection:  changeNav ? 'row': "column"}}>
-            <Image onMouseLeave={() => setGray("grayscale(0%)")} onMouseEnter={() => setGray("grayscale(100%)")} src={profilePic} style={{filter: gray, marginRight: 15, marginTop: -10, cursor: "pointer", transition: "1s"}} height={70} alt={""}    />
+        <view style={{alignItems: "center", minHeight: changeNav ? "20px":"100%", zIndex: 2, minWidth: changeNav ? "100%":"20px", top: changeNav ? null:0, bottom:changeNav ? 0:null, right: 0, display: "flex", position: "fixed", justifyContent: "space-between", flexDirection:  changeNav ? 'row': "column"}}>
+            <Image onMouseLeave={() => setGray("grayscale(0%)")} onMouseEnter={() => setGray("grayscale(100%)")} src={profilePic} style={{filter: gray, marginTop: -10, cursor: "pointer", transition: "1s"}} height={windowSize[1] < 600 ? 50:70} alt={""}    />
             <h1 onMouseEnter={() => setLinkedInHover(1)} onMouseLeave={() => setLinkedInHover(0)} style={{writingMode: changeNav ? "horizontal-tb":"vertical-lr", cursor: 'pointer'}}><span style={{color: "white", transition: "1.0s", opacity:linkedinHover}}>‧₊˚</span><a href="https://www.linkedin.com/in/rachael-mathew-227a18138/" style={{color: "white", textDecoration: "none"}}>linkedIn</a><span style={{color: "white", transition: "1.0s", opacity:linkedinHover}}>˚₊‧</span></h1>
             <h1 onMouseEnter={() => setFBHover(1)} onMouseLeave={() => setFBHover(0)} style={{writingMode: changeNav ? "horizontal-tb":"vertical-lr", cursor: 'pointer'}}><span style={{color: "white", transition: "1.0s", opacity:fbHover}}>‧₊˚</span><a href="https://www.facebook.com/abby.sherman.14268/" style={{color: "white", textDecoration: "none"}}>facebook</a><span style={{color: "white", transition: "1.0s", opacity:fbHover}}>˚₊‧</span></h1>
             <h1 onMouseEnter={() => setInstaHover(1)} onMouseLeave={() => setInstaHover(0)} style={{writingMode: changeNav ? "horizontal-tb":"vertical-lr", cursor: 'pointer'}}><span style={{color: "white", transition: "1.0s", opacity:intstaHover}}>‧₊˚</span><a href="https://www.instagram.com/rachaelsmathew/" style={{color: "white", textDecoration: "none"}}>insta</a><span style={{color: "white", transition: "1.0s", opacity:intstaHover}}>˚₊‧</span></h1>
-            <Image onMouseLeave={() => setGray("grayscale(0%)")} onMouseEnter={() => setGray("grayscale(100%)")} src={profilePic} style={{filter: gray, marginRight: 15, cursor: "pointer", transition: "1s"}} height={70} alt={""}    />
+            <Image onMouseLeave={() => setGray("grayscale(0%)")} onMouseEnter={() => setGray("grayscale(100%)")} src={profilePic} style={{filter: gray, cursor: "pointer", transition: "1s"}} height={windowSize[1] < 600 ? 50:70} alt={""}    />
         </view>
     )
 }
