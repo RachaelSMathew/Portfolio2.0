d5cb870ced375b7afe2edb3a351eaa7526750e4c
Rachael Mathew <rachaelmathew1@gmail.com>
2024-07-03 12:43:27 -0400

    installed vara, removed gsap

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image'
 import jobs from './jobs.png'
 import skills from './skills.png'
 import background from './backgroundImage.png'
-import gsap from "gsap";
 import VaraText from './components/VaraText'
 import Jobs from "./jobs"
 import Literature from "./literature"
@@ -15,24 +14,6 @@ export default function Page() {
   const [transformProjects, setTransformProjects] = useState(1)
   const [transformBooks, setTransformBooks] = useState(1)
   const [transformJobs, setTransformJobs] = useState(1)
-
-  gsap.set('span', { width: 0 })
-
-  gsap.timeline({
-      scrollTrigger:{
-        trigger: 'span',
-        scrub:0.3,
-        start:"top 10%",
-        end:"top 20%",
-        //markers:true
-      }
-  })
-    .to('span', {
-    width:'100%', 
-    duration:1, ease:'none',
-    stagger:1
-  })
-
 
   const [windowSize, setWindowSize] = useState([
     window.innerHeight,
