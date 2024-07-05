73a5787adc05eb7dbf529947650ae43a7eca4671
Rachael Mathew <rachaelmathew1@gmail.com>
2024-07-05 16:41:20 -0400

    reduced text

--- app/jobs.tsx
+++ app/jobs.tsx
@@ -19,15 +19,10 @@ export default function Jobs({transformBooks, transformProjects, transformJobs, 
           <div style={{position: "absolute", transition: ".5s", left: 30, top: 60, right: 30, color: "black" }}>
             <h1 style={{fontSize :"20px", fontWeight: "bold"}}>Flux Marine, Front End Web Developer<br/><h1 style={{fontSize:"15px"}}>Feb 2024 - June 2024</h1></h1>
             &nbsp;&nbsp;&nbsp;&nbsp;Developed front end of a progressive web app using React Native that allows users to track their boat’s location, past trips, speed, battery, voltage, and other metrics. 
-            <br/>&nbsp;&nbsp;&nbsp;&nbsp;Used service workers to create a connectivity-independent app that caches data to speed up information retrieval. 
-            <br/>&nbsp;&nbsp;&nbsp;&nbsp;Worked with AWS Amplify to test and deploy the app and AWS Dynamodb to store user data.
             <h1 style={{fontSize :"20px", fontWeight: "bold"}}>GameChanger, iOS Developer Intern<br/><h1 style={{fontSize:"15px"}}>June 2023 - Aug 2023</h1></h1>
             &nbsp;&nbsp;&nbsp;&nbsp;Enhanced GameChanger app by adding linkable buttons using RxSwift, replacing directional icons as PDF files using SnapKit, adding/editing "help & feedback" buttons/snackbars, and realigning misplaced icons.
-            <br/>&nbsp;&nbsp;&nbsp;&nbsp;Developed an onboarding tutorial using Swift for multiple user roles with animated images accessible during account creation and through a button in the app.
-            <br/>&nbsp;&nbsp;&nbsp;&nbsp;Refactored and updated deprecated API Endpoints in the Backend to retrieve a user's video clips.
             <h1 style={{fontSize :"20px", fontWeight: "bold"}}>iRobot, Software DevOps Co-op<br/><h1 style={{fontSize:"15px"}}>June 2022 - Dec 2022</h1></h1>
             &nbsp;&nbsp;&nbsp;&nbsp;Designed Jenkins pipelines using Groovy and EC2 instances and executed tasks in a Docker container using an ECR image to automate tasks such as configuring repositories, uploading build files to Artifactory, and syncing files.
-            <br/>&nbsp;&nbsp;&nbsp;&nbsp;Developed a feature for Bitbucket pull request pages using ScriptRunner and Groovy, which sends API requests from Bitbucket to Jenkins to trigger a pipeline. Experimented with AWS API Gateway and Lambda to send requests
             </div>
           </div>}
         </div>
