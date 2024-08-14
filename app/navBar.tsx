73a7fb2e515eaa4d5ea3db62a17d50985a3485c7
Rachael Mathew <rachaelmathew1@gmail.com>
2024-08-14 19:41:24 -0400

    updated navbar when screen shortens to smallest width

--- app/navBar.tsx
+++ app/navBar.tsx
@@ -10,6 +10,7 @@ export default function NavBar() {
     const [linkedinHover, setLinkedInHover] = useState(0)
     const [intstaHover, setInstaHover] = useState(0)
     const [fbHover, setFBHover] = useState(0)
+    const [emailHover, setEmailHover] = useState(0)
     
     useEffect(() => {
         if (typeof window === "undefined") return
@@ -29,12 +30,13 @@ export default function NavBar() {
     }, []);
 
     return (
-        <view style={{alignItems: "center", minHeight: changeNav ? "20px":"100%", zIndex: 2, minWidth: changeNav ? "100%":"20px", top: changeNav ? null:0, bottom:changeNav ? 0:null, right: 0, display: "flex", position: "fixed", justifyContent: "space-between", flexDirection:  changeNav ? 'row': "column"}}>
-            <Image onMouseLeave={() => setGray("grayscale(0%)")} onMouseEnter={() => setGray("grayscale(100%)")} src={profilePic} style={{filter: gray, marginTop: -10, cursor: "pointer", transition: "1s"}} height={windowSize[1] < 600 ? 50:70} alt={""}    />
-            <h1 onMouseEnter={() => setLinkedInHover(1)} onMouseLeave={() => setLinkedInHover(0)} style={{writingMode: changeNav ? "horizontal-tb":"vertical-lr", cursor: 'pointer'}}><span style={{color: "white", transition: "1.0s", opacity:linkedinHover}}>‧₊˚</span><a href="https://www.linkedin.com/in/rachael-mathew-227a18138/" style={{color: "white", textDecoration: "none"}}>linkedIn</a><span style={{color: "white", transition: "1.0s", opacity:linkedinHover}}>˚₊‧</span></h1>
+        <view style={{alignItems: "center", backgroundColor: windowSize[1] < 600 ?"#94a2a8":"transparent", minHeight: changeNav ? "20px":"100%", zIndex: 2, minWidth: changeNav ? "100%":"20px", top: changeNav ? null:0, bottom:changeNav ? 0:null, right: 0, display: "flex", position: "fixed", justifyContent: "space-between", flexDirection:  changeNav ? 'row': "column"}}>
+              {windowSize[1] >= 600 && <Image onMouseLeave={() => setGray("grayscale(0%)")} onMouseEnter={() => setGray("grayscale(100%)")} src={profilePic} style={{filter: gray, marginTop: -10, cursor: "pointer", transition: "1s"}} height={windowSize[1] < 600 ? 50:70} alt={""}    />}
+              <h1 onMouseEnter={() => setLinkedInHover(1)} onMouseLeave={() => setLinkedInHover(0)} style={{writingMode: changeNav ? "horizontal-tb":"vertical-lr", cursor: 'pointer'}}><span style={{color: "white", transition: "1.0s", opacity:linkedinHover}}>‧₊˚</span><a href="https://www.linkedin.com/in/rachael-mathew-227a18138/" style={{color: "white", textDecoration: "none"}}>linkedIn</a><span style={{color: "white", transition: "1.0s", opacity:linkedinHover}}>˚₊‧</span></h1>
             <h1 onMouseEnter={() => setFBHover(1)} onMouseLeave={() => setFBHover(0)} style={{writingMode: changeNav ? "horizontal-tb":"vertical-lr", cursor: 'pointer'}}><span style={{color: "white", transition: "1.0s", opacity:fbHover}}>‧₊˚</span><a href="https://www.facebook.com/abby.sherman.14268/" style={{color: "white", textDecoration: "none"}}>facebook</a><span style={{color: "white", transition: "1.0s", opacity:fbHover}}>˚₊‧</span></h1>
             <h1 onMouseEnter={() => setInstaHover(1)} onMouseLeave={() => setInstaHover(0)} style={{writingMode: changeNav ? "horizontal-tb":"vertical-lr", cursor: 'pointer'}}><span style={{color: "white", transition: "1.0s", opacity:intstaHover}}>‧₊˚</span><a href="https://www.instagram.com/rachaelsmathew/" style={{color: "white", textDecoration: "none"}}>insta</a><span style={{color: "white", transition: "1.0s", opacity:intstaHover}}>˚₊‧</span></h1>
-            <Image onMouseLeave={() => setGray("grayscale(0%)")} onMouseEnter={() => setGray("grayscale(100%)")} src={profilePic} style={{filter: gray, cursor: "pointer", transition: "1s"}} height={windowSize[1] < 600 ? 50:70} alt={""}    />
+            <h1 onMouseEnter={() => setEmailHover(1)} onMouseLeave={() => setEmailHover(0)} style={{writingMode: changeNav ? "horizontal-tb":"vertical-lr", cursor: 'pointer'}}><span style={{color: "white", transition: "1.0s", opacity:emailHover}}>‧₊˚</span><a href="mailto: rachaelmathew1@gmail.com" style={{color: "white", textDecoration: "none"}}>email</a><span style={{color: "white", transition: "1.0s", opacity:emailHover}}>˚₊‧</span></h1>
+            {windowSize[1] >= 600  && <Image onMouseLeave={() => setGray("grayscale(0%)")} onMouseEnter={() => setGray("grayscale(100%)")} src={profilePic} style={{filter: gray, cursor: "pointer", transition: "1s"}} height={windowSize[1] < 600 ? 50:70} alt={""}    />}
         </view>
     )
 }
