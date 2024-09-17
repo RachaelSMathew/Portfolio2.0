c5aeddda75a8830803b4402ddac7d316f375607e
Rachael Mathew <rachaelmathew1@gmail.com>
2024-09-16 22:09:26 -0400

    Revert "Update jobs.tsx"
    This reverts commit d3b32be7b3387c1acfcb62756659aa477c5881bc.

--- app/jobs.tsx
+++ app/jobs.tsx
@@ -47,33 +47,33 @@ export default function Jobs({transformBooks, transformProjects, transformJobs, 
     
     return (
       (transformProjects !== 1.6 && transformBooks!==1.8) && <div onClick={()=>{transformJobs!==1.3 && setTransformJobs(1.3)}}  onMouseLeave={() => {if(transformJobs===1.1) setTransformJobs(1)}} onMouseEnter={() => {if(transformJobs==1) setTransformJobs(1.1)}}  style={{cursor: transformJobs !== 1.3 ? "pointer":"default", position: "relative", transition: "1.5s", transform: `scale(${transformJobs})`}}>
-        <Image style={{display: transformProjects!== 1.6 && transformBooks!==1.8? "block":"none", transition: "all .5s ease-out", opacity: transformJobs === 1.3 ? 0.65:1}} src={portfolio} width={400} alt={""}    />
-        <div onMouseLeave={() => transformJobs!==1.3&&setOpacityImageJobs(0)} onMouseEnter={() => transformJobs!==1.3&&setOpacityImageJobs(0.6)} style={{position: "absolute", top: 80, left: 85, height: "62%", width: "54%", transition: ".5s ease", borderRadius: "210px /290px", backgroundColor: `  rgba(178,157,103,${opacityImageJobs})`}}>
+        <Image style={{display: transformProjects!== 1.6 && transformBooks!==1.8? "block":"none", transition: "all .5s ease-out", opacity: transformJobs === 1.3 ? 0.65:1}} src={portfolio} width={500} alt={""}    />
+        <div onMouseLeave={() => transformJobs!==1.3&&setOpacityImageJobs(0)} onMouseEnter={() => transformJobs!==1.3&&setOpacityImageJobs(0.6)} style={{position: "absolute", top: 100, left: 115, height: "62%", width: "54%", transition: ".5s ease", borderRadius: "210px /290px", backgroundColor: `  rgba(178,157,103,${opacityImageJobs})`}}>
           {transformJobs !== 1.3 ? 
           <>
           {/** curved jobs section title when hover over source: https://css-tricks.com/snippets/svg/curved-text-along-path/ */}
           <svg viewBox="0 0 500 500">
             <path style={{fill: "transparent"}} id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
-            <text width="400">
+            <text width="500">
               <textPath style={{opacity: opacityImageJobs === 0?0:1, position: "absolute", transition: ".5s", top: 0, left: 50, fontWeight: "900", fill: "white", fontSize: "75px"}} xlinkHref="#curve">
                 past occupations
               </textPath>
             </text>
           </svg>
           <h1 style={{opacity: opacityImageJobs === 0?0:1, position: "absolute", transition: ".5s", top: "70%", left: 70, color: "white", fontSize :"20px", }}>The Martyr of <br/>the Solway(1871)</h1></>:
-          <div style={{overflow: "scroll", overflowX: "hidden", borderRadius: "210px /290px", width: "230px", height: "297px", position: "relative", backgroundColor: "rgba(190,172,124, 0.3)"}}>
-          <div style={{position: "absolute", cursor: "pointer", transition:".5s", zIndex: 3, top: 0, right: 110, color: "black" }} onClick={()=>{setOpacityImageJobs(0); setScrollJobs(0); scrollVar = 0;  setTransformJobs(1)}} ><h1 style={{fontSize: "30px"}}>X</h1></div>
+          <div style={{overflow: "scroll", overflowX: "hidden", borderRadius: "210px /290px", width: "270px", height: "372px", position: "relative", backgroundColor: "rgba(190,172,124, 0.3)"}}>
+          <div style={{position: "absolute", cursor: "pointer", transition:".5s", zIndex: 3, top: 0, right: 130, color: "black" }} onClick={()=>{setOpacityImageJobs(0); setScrollJobs(0); scrollVar = 0;  setTransformJobs(1)}} ><h1 style={{fontSize: "30px"}}>X</h1></div>
           <p ref={highlightTextRef} style={{position: "absolute",transition: ".5s", left: 30, top: 30, right: 30, color: "black" }}>
             {/** scrollJobs can only be compared to decimals */}
             <h1 style={{fontSize :"20px", color: "black", textAlign: "center", fontWeight: "bold"}}>Flux Marine, Front End Web Developer</h1>
             <h1 style={{color: scrollJobs>=44.44?"black":"#574b0f", textAlign: "center", transition: "all .5s ease-out", fontSize:"15px"}}>Feb 2024 - June 2024</h1>
-            <Image style={{position: "absolute", top: 10, left: -20, transition: "all 1.5s ease-out", opacity: opacityFlux}} src={flux} width={460} alt={""}    />
+            <Image style={{position: "absolute", top: 10, transition: "all 1.5s ease-out", opacity: opacityFlux}} src={flux} width={460} alt={""}    />
             <h1 style={{fontSize :"15px", position: "relative", color: scrollJobs>=60.44?"black":"#574b0f", transition: "all .5s ease-out"}} >&nbsp;&nbsp;&nbsp;&nbsp;Developed front end of a progressive web app using <span style={{color: "#f1c6bf", fontSize: "18px"}}>React Native</span> that allows users to track their boat’s location, past trips, speed, battery, voltage, and other metrics.</h1>
             <h1 style={{fontSize :"20px", color: scrollJobs>=145.44?"black":"#574b0f", transition: "all .5s ease-out", textAlign: "center", fontWeight: "bold"}}>GameChanger, iOS Developer Intern<br/><h1 style={{fontSize:"15px", color: scrollJobs>=175.44?"black":"#574b0f", transition: "all .5s ease-out", textAlign: "center", }}>June 2023 - Aug 2023</h1></h1>
             <Image style={{position: "absolute", top: 270, right: 40, transition: "all 2s ease-out", opacity: !firstScroll ? 0.45 : scrollJobs>=215.44?0.45:0}} src={gameChanger} width={260} alt={""}    />
             <h1 style={{fontSize :"15px", left: 15, position: "relative", color: scrollJobs>=215.44?"black":"#574b0f", transition: "all .5s ease-out"}} >&nbsp;&nbsp;&nbsp;&nbsp;Enhanced GameChanger app by adding linkable buttons using Rx<span style={{color: "#f1c6bf", fontSize: "18px"}}>Swift</span>, replacing directional icons as PDF files using SnapKit, adding/editing "help & feedback" buttons/snackbars, and realigning misplaced icons.</h1>
             <h1 style={{fontSize :"20px", textAlign: "center", fontWeight: "bold", color: scrollJobs>=335.44?"black":"#574b0f", transition: "all .5s ease-out"}}>iRobot, Software DevOps Co-op<br/><h1 style={{fontSize:"15px", textAlign: "center", zIndex: 1, position: "relative", color: scrollJobs>=355.44?"black":"#574b0f", transition: "all .5s ease-out"}}>June 2022 - Dec 2022</h1></h1>
-            <Image style={{position: "absolute", top: 530, left: 20, transition: "all 2s ease-out", opacity: !firstScroll ? 0.7 : scrollJobs>=397.44?0.7:0}} src={iRobot} width={260} alt={""}    />
+            <Image style={{position: "absolute", top: 470, left: 20, transition: "all 2s ease-out", opacity: !firstScroll ? 0.7 : scrollJobs>=397.44?0.7:0}} src={iRobot} width={260} alt={""}    />
             <h1 style={{fontSize :"15px", position: "relative", fontWeight: "bold", color: scrollJobs>=397.44?"black":"#574b0f", transition: "all .5s ease-out"}}>&nbsp;&nbsp;&nbsp;&nbsp;Designed <span style={{color: "#f1c6bf", fontSize: "18px"}}>Jenkins</span> pipelines using <span style={{color: "#f1c6bf", fontSize: "18px"}}>Groovy</span> and <span style={{color: "#f1c6bf", fontSize: "18px"}}>EC2</span> instances and executed tasks in a Docker container using an ECR image to automate tasks such as configuring repositories, uploading build files to Artifactory, and syncing files.</h1>
           </p>
           </div>}
